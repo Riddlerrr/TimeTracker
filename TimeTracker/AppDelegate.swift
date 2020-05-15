c9aa73a6138994b0aac90df50b9861ffdf92ab50
Sergey Andronov <riddlerrr@gmail.com>
2020-05-15 09:36:25 +0300

    Initial Commit

---
+++ TimeTracker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TimeTracker
+//
+//  Created by Андронов Сергей on 15/05/2020.
+//  Copyright © 2020 Sergey Andronov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TimeTracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TimeTracker
+//
+//  Created by Андронов Сергей on 15/05/2020.
+//  Copyright © 2020 Sergey Andronov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
